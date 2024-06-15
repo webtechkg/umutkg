7691ae8bcf7d74f2e6762de15c0a973b6120edac
woidead <abikbest05@gmail.com>
2024-06-15 02:17:08 -0500

    testing models

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('apps.main.urls')),
+    path('', include('apps.main.urls')),
     path('tests/', include('apps.tests.urls')),
 ]
 
